e0d2759eea17a7618011eab73ed0d29be90a5972
erik maggio <erik97@hotmail.com.ar>
2023-01-14 15:12:25 -0300

    implementacion de dependecias navigation , creacion de navigate + fragments , creacion de carpetas, creacion de homelogin en visual y navegacion hacia 2 puntos , login y terms and conditions segun el figma

---
+++ app/src/main/java/com/example/genericproyect/HomeLoginFragment.kt
@@ -0,0 +1,36 @@
+package com.example.genericproyect
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.genericproyect.databinding.FragmentHomeLoginBinding
+
+
+class HomeLoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeLoginBinding.inflate(inflater, container, false)
+
+        navigation()
+
+        return binding.root
+    }
+
+    private fun navigation() {
+        binding.btLogin.setOnClickListener {
+            findNavController().navigate(R.id.loginFragment)
+        }
+
+        binding.btCreate.setOnClickListener {
+            findNavController().navigate(R.id.termsAndConditionsFragment)
+        }
+    }
+}
