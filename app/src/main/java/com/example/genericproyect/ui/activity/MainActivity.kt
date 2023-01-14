37dd23cdd5a79edbdb41acdafc18bfd7190a217e
erik maggio <erik97@hotmail.com.ar>
2023-01-14 14:02:26 -0300

    incorporacion de splash con diseño

--- app/src/main/java/com/example/genericproyect/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/genericproyect/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.genericproyect
+package com.example.genericproyect.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.genericproyect.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
